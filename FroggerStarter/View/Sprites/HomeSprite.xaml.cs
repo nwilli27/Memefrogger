75026574b09c9b0a318a22fb50257bded1aad7cf
Nolan Williams <nwilli27@my.westga.edu>
2019-10-27 22:05:47 -0400

    Added HomeLocation Graphics to gamePage and created HomeSprite Close #8

---
+++ FroggerStarter/View/Sprites/HomeSprite.xaml.cs
@@ -0,0 +1,19 @@
+﻿
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace FroggerStarter.View.Sprites
+{
+    public sealed partial class HomeSprite
+    {
+
+        #region Constructors
+
+        public HomeSprite()
+        {
+            this.InitializeComponent();
+        }
+
+        #endregion
+    }
+}
