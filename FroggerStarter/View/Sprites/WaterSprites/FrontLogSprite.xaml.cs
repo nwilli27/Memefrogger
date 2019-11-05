935b2ab13b707efe62a04a846a7ac560a50d1464
Nolan Williams <nwilli27@my.westga.edu>
2019-11-05 12:24:46 -0500

    Added 2 water sprites and adjusted sprite namespaces Close #43

---
+++ FroggerStarter/View/Sprites/WaterSprites/FrontLogSprite.xaml.cs
@@ -0,0 +1,23 @@
+﻿
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace FroggerStarter.View.Sprites
+{
+    /// <summary>
+    ///     Truck sprite class.
+    /// </summary>
+    /// <seealso cref="BaseSprite" />
+    /// <seealso cref="Windows.UI.Xaml.Markup.IComponentConnector" />
+    /// <seealso cref="Windows.UI.Xaml.Markup.IComponentConnector2" />
+    public sealed partial class FrontLogSprite
+    {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="FrontLogSprite"/> class.
+        /// </summary>
+        public FrontLogSprite()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
