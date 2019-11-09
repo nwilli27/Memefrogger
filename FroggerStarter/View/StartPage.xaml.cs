a790c4c12d3b3d9b8a04c7c7c54d95e27db1f455
Thomas Morgenstern <tmorgen1@my.westga.edu>
2019-11-09 02:30:38 -0500

    Created StartPage.xaml and StartPage.xaml.cs.  Game now also starts on the StartPage and moves into the GamePage when Start button is pressed.

---
+++ FroggerStarter/View/StartPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace FroggerStarter.View
+{
+    /// <summary>
+    ///     An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class StartPage : Page
+    {
+        #region Constructors
+
+        public StartPage()
+        {
+            this.InitializeComponent();
+        }
+
+        #endregion
+
+        private void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(GamePage));
+        }
+    }
+}
